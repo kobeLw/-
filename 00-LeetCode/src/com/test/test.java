fdfcf081eb3a75802c79aad6e103e7e2e85e7db2
reus <275631849@qq.com>
2020-05-09 17:38:51 +0800

    LeetCode练习

--- 00-LeetCode/src/com/test/test.java
+++ 00-LeetCode/src/com/test/test.java
@@ -1,5 +1,9 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class test {
 
 	public static void main(String[] args) {
@@ -8,27 +12,34 @@ public class test {
 		
 		
 		test test = new test();
-		int [] arr = new int[] {0,1,0,2,1,0,1,3,2,1,2,1};
-		System.out.println(test.trap(arr));
+		int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+		System.out.println(test.dailyTemperatures(temperatures));
 		
 	}
 	
-	public int trap(int[] height) {
-		if (height.length == 0) {
-			return 0;
-		}
-		int total = 0;
-		int left = height[0];
-		int right = 0;
-		for (int i = 1; i < height.length - 1; i++) {
-			right = height[i];
-			if (right < left) {
-				total += (left - right);
-			} else {
-				left = right;
+	/**
+	 * 739.每日温度
+	 * 根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+	 * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]
+	 * @param T
+	 * @return
+	 */
+	public int[] dailyTemperatures(int[] T) {
+		Stack<Integer> stack = new Stack<Integer>();
+		int[] res = new int[T.length];
+		
+		for (int i = 0; i < T.length; i++) {
+			while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+				res[stack.peek()] = i - stack.peek();
+				stack.pop();
 			}
+			
+			stack.push(i);
 		}
-		return total;
-    }
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
+		return res;
+	}
 
 }
